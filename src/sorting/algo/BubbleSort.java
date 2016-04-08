52bf3472fd7867d33d58df2e8ad6f55e429e0515
dvp236 <dvp236@nyu.edu>
2016-04-07 23:37:49 -0400

    *Added comments - BubbleSort

--- src/sorting/algo/BubbleSort.java
+++ src/sorting/algo/BubbleSort.java
@@ -2,15 +2,24 @@ package sorting.algo;
 
 import java.util.Arrays;
 
+/**
+ * 
+ * @author dharmik
+ * Implement BubbleSort
+ */
 public class BubbleSort {
 	
+	//Time Complexity: O(n^2)
 	public static int[] sort(int[] arr){
-		int temp;
-		
+		//Bubble sort put the largest value to end of list
         for(int i=0; i < arr.length-1 ; i++){
         	
             for(int j=1; j < arr.length-i; j++){
+            	//compare each element compare it with previous element
                 if(arr[j-1] > arr[j]){
+                	
+                	//swap values
+                	int temp;
                     temp=arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
@@ -19,13 +28,13 @@ public class BubbleSort {
             }
 
         }
+        
         return arr;
 	}
 	
 	public static void main(String[] args) {
-		 
-        int arrayList[] = { 5,3,9,7,1,8,2 };
-        System.out.println("\nFinal result:"+Arrays.toString(sort(arrayList)));
+		int arrayList[] = { 5,3,9,7,1,8,2 };
+        System.out.println("Final result:"+Arrays.toString(sort(arrayList)));
 	}
 
 }
