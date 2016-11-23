3d63175e0661cd68ca37b8c9eda8815ca3d6deac
Dharmik Patel <dvp236@nyu.edu>
2016-11-23 12:13:53 -0500

    update heapsort

--- src/sorting/algo/HeapSort.java
+++ src/sorting/algo/HeapSort.java
@@ -6,7 +6,31 @@ package sorting.algo;
  */
 
 public class HeapSort {
-
+	private int[] heap;
+	private int size;
+	private void buildHeap(){
+		
+	}
+	private void percolateDown(int pos){
+		
+	}
+	public void heapSort(int[] a){
+		size=a.length;
+		heap = new int[size+1];
+		System.arraycopy(a,0,heap,1,size);
+		buildHeap();
+		
+		for(int k=size; k>0;k--){
+			int temp = heap[k];
+			heap[k] = heap[1];
+			heap[1] = temp;
+			//size--;
+			percolateDown(1);
+		}
+		for(int k = 1; k < heap.length-1; k++) 
+	         a[k] = heap[heap.length - 1 - k];
+		//printHeap();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
