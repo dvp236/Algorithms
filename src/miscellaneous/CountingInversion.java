16183331d8a5ac2ee51980761a7d980359640445
Dharmik Patel <dvp236@nyu.edu>
2017-01-16 05:11:08 -0500

    *added counting inversion

---
+++ src/miscellaneous/CountingInversion.java
@@ -0,0 +1,21 @@
+package miscellaneous;
+
+/**
+ * 
+ * @author dharmik
+ * 
+ * To Count Inversions we will use the help of mergesort
+ * Right modification in the mergesort code can give us number of inversions
+ * 
+ * One of the main Applications of counting inversion is getting similar profile in 
+ * recommendations system. The profile which has minimum number of inversion with yours can 
+ * help determine your taste in shopping and browsing
+ * 
+ */
+
+public class CountingInversion {
+	
+	public static void main(String[] args){
+		
+	}
+}
