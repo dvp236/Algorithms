a29cbcd4d077405fcd8c043814a0f20a69866c84
Dharmik Patel <dvp236@nyu.edu>
2017-03-09 21:28:37 -0500

    Adding solution for successor problem

--- src/applications/algo/SuccessorWithDelete.java
+++ src/applications/algo/SuccessorWithDelete.java
@@ -1,5 +1,7 @@
 package applications.algo;
 
+import java.util.Arrays;
+
 /**
  * 
  * @author dharmik
@@ -26,7 +28,7 @@ class WQUWithFindLargest{
 		for(int i=0; i<N; i++){
 			points[i] = i;
 			sz[i] = 1;
-			large[i] = i;	
+			large[i] = i;
 		}
 	}
 	
@@ -68,39 +70,89 @@ class WQUWithFindLargest{
 	
 }
 
+class SuccessorFinder{
+
+	boolean[] isRemoved;
+	WQUWithFindLargest wqu;
+	int N;
+	public SuccessorFinder(int N){
+		//initially all values false
+		this.N = N;
+		isRemoved = new boolean[N];
+		wqu = new WQUWithFindLargest(N);
+	}
+	
+	public void remove(int a){
+		if(isRemoved[a]) return;
+		isRemoved[a] = true;
+		//if lesser value is removed union them together
+		if(a > 0 && isRemoved[a-1]) wqu.union(a, a-1);
+		
+		//if greater value is removed union them together
+		if(a < N-1 && isRemoved[a+1]) wqu.union(a, a+1);
+			
+	}
+	
+	public int successor(int a){
+		if(a >= N){
+			return -1;
+		}
+		
+		if(isRemoved[a]){
+			//0 1 2 3 4 5 6
+			//T F F F T F T
+			//here we find the largest of removed values, and adding 1 more gives bigger non removed. thats successor
+			int successor = wqu.find(a) + 1;
+			return successor;
+		}
+		return a;
+	}
+	
+	
+}
+
 public class SuccessorWithDelete {
 
 	public static void main(String[] args) {
 		
-		WQUWithFindLargest qu = new WQUWithFindLargest(12);
-		qu.union(0, 4);
-		qu.union(2, 3);
-		qu.union(1, 4);
-		qu.union(2, 4);
-		
-		qu.union(7, 5);
-		qu.union(5, 11);
-		qu.union(7, 10);
-		qu.union(6, 10);
-		
-		
-		
-		System.out.println(qu.find(2));
-		System.out.println(qu.find(1));
-		System.out.println(qu.find(0));
-		System.out.println(qu.find(4));
-		System.out.println("----");
-		
-		System.out.println(qu.find(5));
-		System.out.println(qu.find(6));
-		System.out.println(qu.find(11));
-		System.out.println(qu.find(10));
-		System.out.println(qu.find(7));
-		
-		System.out.println(qu.connected(0,3));
-		System.out.println(qu.connected(6,5));
-		System.out.println(qu.connected(6,4));
+//		WQUWithFindLargest qu = new WQUWithFindLargest(12);
+//		qu.union(0, 4);
+//		qu.union(2, 3);
+//		qu.union(1, 4);
+//		qu.union(2, 4);
+//		
+//		qu.union(7, 5);
+//		qu.union(5, 11);
+//		qu.union(7, 10);
+//		qu.union(6, 10);
+//		
+//		
+//		
+//		System.out.println(qu.find(2));
+//		System.out.println(qu.find(1));
+//		System.out.println(qu.find(0));
+//		System.out.println(qu.find(4));
+//		System.out.println("----");
+//		
+//		System.out.println(qu.find(5));
+//		System.out.println(qu.find(6));
+//		System.out.println(qu.find(11));
+//		System.out.println(qu.find(10));
+//		System.out.println(qu.find(7));
+//		
+//		System.out.println(qu.connected(0,3));
+//		System.out.println(qu.connected(6,5));
+//		System.out.println(qu.connected(6,4));
 	
+		
+		SuccessorFinder s = new SuccessorFinder(7);
+		//System.out.println(s.successor(5));
+		
+		s.remove(2);
+		s.remove(3);
+		s.remove(1);
+		s.remove(5);
+		System.out.println(s.successor(2));
 
 	}
 
