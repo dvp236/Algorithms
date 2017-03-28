6c3d9f7190f139f5ff67ffa57efde9fcc1d7677d
Dharmik Patel <dvp236@nyu.edu>
2017-03-28 19:04:37 -0400

    Added bitonicSearch

---
+++ src/search/algo/SearchBitonicArray.java
@@ -0,0 +1,19 @@
+package search.algo;
+//http://stackoverflow.com/questions/19372930/given-a-bitonic-array-and-element-x-in-the-array-find-the-index-of-x-in-2logn
+/**
+ * 
+ * @author dharmik
+ * 
+ *
+ */
+
+/*
+ * 
+Search in a bitonic array. An array is bitonic if it is comprised of an increasing sequence of integers followed immediately by a decreasing sequence of integers. Write a program that, given a bitonic array of n distinct integer values, determines whether a given integer is in the array.
+
+Standard version: Use ∼3lgn compares in the worst case.
+Signing bonus: Use ∼2lgn compares in the worst case (and prove that no algorithm can guarantee to perform fewer than ∼2lgn compares in the worst case).
+ */
+public class SearchBitonicArray {
+
+}
