c16ca17961344f27067ed3b457f7a5e5ebc53241
Dharmik Patel <dvp236@nyu.edu>
2017-05-20 22:28:10 -0400

    updated GCD

--- src/miscellaneous/GCD.java
+++ src/miscellaneous/GCD.java
@@ -2,4 +2,13 @@ package miscellaneous;
 
 public class GCD {
 
+	//Euclidean's algorithm
+	public static int gcd(int a, int b) {
+		if( a == 0 ) return b;
+		return gcd( b%a , a);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(gcd(16,24));
+	}
 }
