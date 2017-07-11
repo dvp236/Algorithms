16938a90d850f5b0fc3131cd4d11d0b6aef9ab75
Dharmik Patel <dvp236@nyu.edu>
2017-07-10 20:01:26 -0400

    updated BFS

--- src/graph/algo/BFS.java
+++ src/graph/algo/BFS.java
@@ -1,9 +1,67 @@
 package graph.algo;
 
-public class BFS {
+import edu.princeton.cs.algs4.Queue;
 
+/**
+ * 
+ * @author dharmik
+ * Implement BFS for Undirected Graph.
+ * 
+ *
+ */
+
+public class BFS {
+	private boolean[] markTo;
+	private int[] distTo;
+	
+	public BFS(UndirectedGraph G, int s) {
+		markTo = new boolean[G.V()];
+		distTo = new int[G.V()];
+		bfs(G, s);
+	}
+	
+	public void bfs(UndirectedGraph G, int s) {
+		Queue<Integer> q = new Queue<Integer>();
+		markTo[s] = true;
+		for( int v = 0; v < G.V(); v++) {
+			distTo[v] = Integer.MAX_VALUE;
+		}
+		distTo[s] = 0;
+		q.enqueue(s);
+		
+		while(!q.isEmpty()) {
+			int v = q.dequeue();
+			for (Integer w: G.adj(v)) {
+				if(!markTo[w]) {
+					q.enqueue(w);
+					markTo[w] = true;
+					distTo[w] = distTo[v] + 1;
+				}
+			}
+		}
+	}
+	
+	
+	public int distTo(int v) {
+		return distTo[v];
+	}
+
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		UndirectedGraph ug = new UndirectedGraph(5);
+		ug.addEdge(0, 1);
+		ug.addEdge(0, 2);
+		ug.addEdge(1, 3);
+		ug.addEdge(2, 3);
+		ug.addEdge(2, 4);
+		ug.addEdge(3, 4);
+		
+		int source = 0;
+		BFS b = new BFS(ug, source);
+		
+		for(int v = 0; v < ug.V() ; v++) {
+			System.out.println("0 - "+v+" : "+b.distTo(v));
+		}
 
 	}
 
