6ec1ee21c04ed36092e80367cd1dbd156333da76
Dharmik Patel <dvp236@nyu.edu>
2017-07-29 18:36:38 -0400

    Updated deapth first path

--- src/applications/algo/DeapthFirstPaths.java
+++ src/applications/algo/DeapthFirstPaths.java
@@ -1,13 +1,92 @@
 package applications.algo;
+
+
+import java.util.Stack;
+
+import edu.princeton.cs.algs4.DepthFirstDirectedPaths;
+import edu.princeton.cs.algs4.StdOut;
+import graph.algo.UndirectedGraph;
+
 /**
  * 
  * @author dharmik
  *
  */
 public class DeapthFirstPaths {
+	private boolean[] marked;
+	private int[] edgeTo; //edge to previous vertex;
+	private final int s;
+	
 
+	public DeapthFirstPaths(UndirectedGraph G, int s) {
+		this.s = s;
+		edgeTo = new int[G.V()];
+		marked = new boolean[G.V()];
+		edgeTo[s] = s;
+		dfs(G,s);
+	}
+	
+	private void dfs(UndirectedGraph G, int v) {
+		marked[v] = true;
+		for(int w : G.adj(v)) {
+			if(!marked[w]) {
+				edgeTo[w] = v;
+				dfs(G, w);
+			}
+		}
+	}
+	
+	public boolean hasPathTo(int v) {
+		validateVertex(v);
+		return marked[v];
+	}
+	
+	public Iterable<Integer> pathTo(int v) {
+		validateVertex(v);
+		if(!hasPathTo(v)) return null;
+		Stack<Integer> path = new Stack<Integer>();
+		int w = edgeTo[v];
+		while(w != s) {
+			path.push(w);
+			w = edgeTo[w];
+		}
+		path.push(s);
+		return path;
+	}
+	
+	private void validateVertex(int v) {
+		int V = marked.length;
+		if ( v < 0 || v > V - 1 ) throw new IllegalArgumentException("Vertex should be between 0 and "+V);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		UndirectedGraph G = new UndirectedGraph(9);
+		G.addEdge(0, 1);
+		G.addEdge(1, 2);
+		G.addEdge(3, 2);
+		G.addEdge(3, 4);
+		G.addEdge(1, 5);
+		G.addEdge(5, 7);
+		G.addEdge(7, 8);
+//		G.addEdge(2, 4);
+//		G.addEdge(3, 4);
+		int s = 0;
+		DeapthFirstPaths dfs = new DeapthFirstPaths(G, 0);
+		for(int v=0; v < G.V(); v++){
+			if (dfs.hasPathTo(v)) {
+                StdOut.printf("%d to %d:  ", s, v);
+                for (int x : dfs.pathTo(v)) {
+                    if (x == s) StdOut.print(x);
+                    else        StdOut.print( x+"-" );
+                }
+                StdOut.println();
+            }
+
+            else {
+                StdOut.printf("%d to %d:  not connected\n", s, v);
+            }		
+		}
 
 	}
 
